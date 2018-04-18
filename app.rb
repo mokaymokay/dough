0fa01a0e7d60eb19d60e1a7a16cdb6b7e30bbf40
Kay Mok <kaymok3@gmail.com>
2018-04-18 14:27:08 -0400

    Add routes to product pages

--- app.rb
+++ app.rb
@@ -4,3 +4,15 @@ require 'sinatra/reloader'
 get '/' do
   erb :index
 end
+
+get '/:product' do
+  if params[:product] == "cakes"
+    erb :cakes
+  elsif params[:product] == "cookies"
+    erb :cookies
+  elsif params[:product] == "muffins"
+    erb :muffins
+  else
+    redirect to('/')
+  end
+end
