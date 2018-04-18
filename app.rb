aa575bc89619dd4decc6cda00e1abe17690797a3
Kay Mok <kaymok3@gmail.com>
2018-04-18 17:04:16 -0400

    Set up dynamic display of various products in one view

--- app.rb
+++ app.rb
@@ -11,16 +11,23 @@ end
 
 get '/:product' do
   if params[:product] == "cakes"
-    dough.add_cake(101, "Black Forest Cake", 30, "Layers of chocolate sponge cake, cherries, and whipped cream")
-    dough.add_cake(102, "Strawberry Shortcake", 25, "Layers of sponge cake, fresh strawberries, and whipped cream")
-    dough.add_cake(103, "Vanilla Crepe Cake", 35, "Paper-thin handmade crêpes layered with light pastry cream")
-    # p dough.cakes[101]
-    dough.cakes.each
-    erb :product
+    dough.add_cake(101, "Black Forest Cake", 30.00, "Layers of chocolate sponge cake, cherries, and whipped cream")
+    dough.add_cake(102, "Strawberry Shortcake", 25.00, "Layers of sponge cake, fresh strawberries, and whipped cream")
+    dough.add_cake(103, "Vanilla Crepe Cake", 35.00, "Paper-thin handmade crêpes layered with light pastry cream")
+    @products = dough.cakes
+    erb :products
   elsif params[:product] == "cookies"
-    erb :product
+    dough.add_cookie(101, "Black Forest Cake", 30.00, "Layers of chocolate sponge cake, cherries, and whipped cream")
+    dough.add_cookie(102, "Strawberry Shortcake", 25.00, "Layers of sponge cake, fresh strawberries, and whipped cream")
+    dough.add_cookie(103, "Vanilla Crepe Cake", 35.00, "Paper-thin handmade crêpes layered with light pastry cream")
+    @products = dough.cookies
+    erb :products
   elsif params[:product] == "muffins"
-    erb :product
+    dough.add_muffin(101, "Black Forest Cake", 30.00, "Layers of chocolate sponge cake, cherries, and whipped cream")
+    dough.add_muffin(102, "Strawberry Shortcake", 25.00, "Layers of sponge cake, fresh strawberries, and whipped cream")
+    dough.add_muffin(103, "Vanilla Crepe Cake", 35.00, "Paper-thin handmade crêpes layered with light pastry cream")
+    @products = dough.muffins
+    erb :products
   else
     redirect to('/')
   end
